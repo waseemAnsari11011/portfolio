0f255470120ec2ce77a0fb7ed05c59217b918b97
waseemcevious <waseem@cevious.com>
2023-08-08 10:52:55 +0530

    bug partiles

--- src/app/components/ParticleBackground.js
+++ src/app/components/ParticleBackground.js
@@ -25,7 +25,7 @@ const ParticleBackground = () => {
           interactivity: {
             events: {
               onClick: {
-                enable: true,
+                enable: false,
                 mode: "push",
               },
               onHover: {
